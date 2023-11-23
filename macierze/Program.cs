b1cbbb52c591fee6c38b436d8d5833b3d03246f7
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-23 18:04:26 +0100

    Add project files.

---
+++ macierze/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace macierze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            var (n, m) = ( temp[0], temp[1] );
+            int[,] a = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                var wiersz = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = wiersz[j];
+                }
+            }
+            //wypisywanie tablicy kolumnami
+            for (int i = 0; i < a.GetLength(1); i++)
+            {
+                for (int j = 0; j < a.GetLength(0); j++)
+                {
+                    Console.Write(a[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ tablice/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] a;
+a = new int[] { -2, -1, 0, 1, 1, 4 };
+int[] b;
+b = new int[] { -3, -2, -1, 2, 3 };
+
+bool pusto = true;
+for (int i = 0; i < a.Length; i++)
+{
+    bool jest = false;
+    for (int j = 0; j < b.Length; j++)
+    {
+        if (a[i] == b[j])
+        {
+            jest = true;
+            break;
+        }
+        else if (a[i - 1] == b[j])
+        {
+            jest = true;
+        }
+    }
+    if (jest == false)
+    {
+        Console.Write(a[i] + " ");
+        pusto = false;
+    }
+}
+if (pusto == true)
+{
+    Console.WriteLine("empty");
+}
