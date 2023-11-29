9adf7edf9c2ea81c452902f7458bd2906fffe91e
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-29 20:32:51 +0100

    commit

---
+++ tablice-test/Program.cs
@@ -0,0 +1,32 @@
+﻿
+static double Srednia(int[,] tab)
+{
+    if (tab == null || tab.Length == 0)
+    {
+        return 0.00;
+    }
+
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            int value = tab[i, j];
+            if (value > 0)
+            {
+                sum += value;
+                count++;
+            }
+        }
+    }
+
+    if (count == 0)
+    {
+        return 0.00;
+    }
+
+    double average = (double)sum / count;
+    return Math.Round(average, 2);
+}
